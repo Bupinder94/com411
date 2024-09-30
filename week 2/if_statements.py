64645e44f0a70bebc557cf9d994ce6e216aef86c
BHUPINDER SINGH <0singb42@solent.ac.uk>
2024-09-30 15:52:14 +0100

    testing Pycharm with github.

---
+++ week 2/if_statements.py
@@ -0,0 +1,24 @@
+print("what type of book is this")
+typeofbook = input()
+
+if typeofbook == "adventure":
+    print("i like advanture books")
+print("finish readinng book")
+
+print("please enter the activity to be performed")
+activity = input()
+
+if activity == "calculate":
+    print("performing calculations. . .")
+else :
+    print("performing activity. . .")
+
+print("activity completed")
+
+print("towards which direction should i go ")
+direction = input()
+
+if direction == ("up")
+    print(up)
+else :
+    print()
